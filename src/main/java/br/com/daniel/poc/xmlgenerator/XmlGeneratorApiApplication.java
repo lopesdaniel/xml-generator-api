e2e91dfddc09f00aa363e89f4badf974516370d6
Daniel Lopes <viana.daniel02@gmail.com>
2025-07-10 17:06:02 -0300

    first commit

---
+++ src/main/java/br/com/daniel/poc/xmlgenerator/XmlGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.poc.xmlgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlGeneratorApiApplication.class, args);
+	}
+
+}
